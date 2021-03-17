6de8304828eddde245300a7634b2809616b29e7d
JZSweet <sweet_zhaozhao@live.cn>
2021-03-16 22:29:17 -0700

    added range rout

--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,16 @@ const db = require("../models");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  const allExercises = await db.Workout.find();
-  res.json(allExercises);
+    const allExercises = await db.Workout.find();
+    res.json(allExercises);
 });
 
 router.post("/", async (req, res) => {
-    try{
-  const newExercise = await db.Workout.create(req.body);
-  res.status(201);
-  res.send(`Exercises with id: ${newExercise.id} was created!`);
-    }catch(err) {
+    try {
+        const newExercise = await db.Workout.create(req.body);
+        res.status(201);
+        res.send(`Exercises with id: ${newExercise.id} was created!`);
+    } catch (err) {
         res.status('403')
         res.send(`Failed with: ${err}`)
     }
@@ -20,7 +20,11 @@ router.post("/", async (req, res) => {
 router.get("/workouts", async (req, res) => {
     const allExercises = await db.Workout.find();
     res.json(allExercises);
-  });
+});
 
+router.get("/workouts/range", async (req, res) => {
+    const allExercises = await db.Workout.find();
+    res.json(allExercises);
+  });
 
 module.exports = router;
